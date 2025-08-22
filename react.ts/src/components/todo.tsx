d3869cb2ca48662f0b6507680fb8ecb8d8703677
72 <72sevenzy2@gmail.com>
2025-08-22 05:32:37 -0800

    refactored but with simpler code

--- react.ts/src/components/todo.tsx
+++ react.ts/src/components/todo.tsx
@@ -1,57 +1,37 @@
-import React, { useState, useReducer, type JSX } from "react";
+import React, { useState, type JSX } from "react";
 import submitconfig from "../config/submit";
 
-interface Todo { text: string; id: number; }
-interface deltodo { type: "delete"; payload: number }
-interface addtodo { type: "add"; payload: string }
+interface Todo { text: string; id: number };
 
-type ztodo = deltodo | addtodo;
-
-const reducerfunc = (state: Todo[], action: ztodo): Todo[] => {
-    switch (action.type) {
-        case "add":
-            const newtodo: Todo = { text: action.payload, id: Date.now() };
-            return [...state, newtodo];
-            break;
-        case "delete":
-            return state.filter(todo => todo.id !== action.payload);
-            break;
-        default: return state;
-    }
-}
-
-const Todoappp = (): JSX.Element => {
+const Todoapp = (): JSX.Element => {
     const [args, setter] = useState<string>("");
-    const [state, dispatch] = useReducer(reducerfunc, [] as Todo[]);
+    const [int, setint] = useState<Todo[]>([]);
 
-    const buttonsubmit: string = "initialbutton";
+    const btnsub: string = "initialbutton";
 
-    const handleargs = (event: React.FormEvent<HTMLFormElement>): void => {
+    const handledelete = (id: number) => { return int.filter(todo => todo.id !== id); }
+
+    const handleadd = () => {
         if (args.trim() !== "") {
-            dispatch({ type: "add", payload: args }); setter("");
-            event.preventDefault(); console.log("rendered task"); // for debugging purposes
-        }
+            return setint([...int, { text: args, id: Date.now() }]);
+        } setter("");
     }
 
     return (
-        <>
-            <form onSubmit={handleargs}>
-                <h1>to do app</h1>
-                <input type="text" value={args} onChange={(event) => { setter(event.target.value) }}
-                    placeholder="Add a task" aria-label="Add a task" onKeyDown={(event) => submitconfig(event, buttonsubmit)} />
-                <button id={buttonsubmit} type="submit">Add</button>
-            </form>
+        <div>
+            <input type="text" value={args} onChange={(event) => setter(event.target.value)}
+                onKeyDown={(event: React.KeyboardEvent) => { submitconfig(event, btnsub) }} />
+            <button id={btnsub} onClick={handleadd}>Add</button>
+
             <ul>
-                {state.map(todo =>
-                    <li key={todo.id}>{todo.text} {" "}
-                        <button aria-label="Delete a task" onClick={() => dispatch({ type: "delete", payload: todo.id })}>
-                            DEL
-                        </button>
+                {int.map(todo =>
+                    <li key={todo.id}>{todo.text}
+                        <button onClick={() => handledelete(todo.id)}></button>
                     </li>
                 )}
             </ul>
-        </>
+        </div>
     )
 }
 
-export default Todoappp;
+export default Todoapp;
