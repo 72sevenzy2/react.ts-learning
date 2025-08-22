00aecbeb191e2f0b9aead30aaa826ed7df4eaf71
72 <72sevenzy2@gmail.com>
2025-08-22 06:05:50 -0800

    refactored again using a reducer

--- react.ts/src/components/todo.tsx
+++ react.ts/src/components/todo.tsx
@@ -1,31 +1,43 @@
-import React, { useState, type JSX } from "react";
+import React, { useReducer, useState, type JSX } from "react";
 import submitconfig from "../config/submit";
 
-interface Todo { text: string; id: number };
+interface Todo { id: number; text: string };
+
+interface deltask { type: "delete"; payload: number };
+interface addtask { type: "add"; payload: string };
+
+type todoargs = deltask | addtask;
+
+const reducerfunc = (state: Todo[], action: todoargs): Todo[] => {
+    switch (action.type) {
+        case "add":
+            return [...state, { id: Date.now(), text: action.payload }];
+        case "delete":
+            return state.filter(todo => todo.id !== action.payload);
+        default: return state;
+    }
+}
 
 const Todoapp = (): JSX.Element => {
     const [args, setter] = useState<string>("");
-    const [int, setint] = useState<Todo[]>([]);
+    const [todos, dispatch] = useReducer(reducerfunc, [] as Todo[]);
 
     const btnsub: string = "initialbutton";
-
-    const handledelete = (id: number) => { setint(int.filter(todo => todo.id !== id)); }
-
-    const handleadd = (): void | undefined => {
-        if (!args.trim()) { return undefined }
-        setint([...int, { text: args, id: Date.now() }]); setter("");
-    }
-
     return (
         <div>
             <input type="text" value={args} onChange={(event) => setter(event.target.value)}
-                onKeyDown={(event: React.KeyboardEvent) => { submitconfig(event, btnsub) }} />
-            <button id={btnsub} onClick={handleadd}>Add</button>
+                onKeyDown={(event: React.KeyboardEvent) => submitconfig(event, btnsub)} />
+            <button id={btnsub} onClick={() => {
+                if (args.trim()) {
+                    dispatch({ type: "add", payload: args }); setter("");
+                }
+            }
+            }>ADD</button>
 
             <ul>
-                {int.map(todo =>
-                    <li key={todo.id}>{todo.text}
-                        <button onClick={() => handledelete(todo.id)}></button>
+                {todos.map(todo =>
+                    <li key={todo.id}>{todo.text} {" "}
+                        <button onClick={() => dispatch({ type: "delete", payload: todo.id })}>DEL</button>
                     </li>
                 )}
             </ul>
