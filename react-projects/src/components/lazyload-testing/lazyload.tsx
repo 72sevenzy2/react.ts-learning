7eab60440e7932408361af5429b73cb8e9626f01
72 <72sevenzy2@gmail.com>
2025-09-13 08:32:48 -0800

    tested lazy loading between 2 components

--- react-projects/src/components/lazyload-testing/lazyload.tsx
+++ react-projects/src/components/lazyload-testing/lazyload.tsx
@@ -3,16 +3,19 @@ import type { JSX } from "react";
 import { useState, Suspense, lazy } from "react";
 
 const Profile = lazy(() => import("./profile"));
+const Desc = lazy(() => import("./desc"));
 
 const Lazyapp = (): JSX.Element => {
-    const [int, args] = useState<boolean>(false);
+    const [int, args] = useState<"desc" | "profile">("desc");
 
     return (
         <div>
-            <button onClick={() => args(true)}>show</button>
+            <button onClick={() => args("desc")}>Description</button>
+            <button onClick={() => args("profile")}>Profile</button>
+
 
             <Suspense fallback={<p>Loading...</p>}>
-                {int && <Profile></Profile>}
+                {int === "desc" ? <Desc></Desc> : <Profile></Profile>}
             </Suspense>
         </div>
     )
