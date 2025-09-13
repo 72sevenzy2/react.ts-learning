2e283686b29a30f634f760a1baa61604f86290a0
72 <72sevenzy2@gmail.com>
2025-09-12 21:49:27 -0800

    updated imports

--- react-projects/src/App.tsx
+++ react-projects/src/App.tsx
@@ -3,11 +3,13 @@ import MutateApp from './components/query'
 import Pokemon from './components/query2'
 import Mutation from './components/mutation'
 
+import Lazyapp from './components/lazyload-testing/lazyload'
+
 function App() {
 
   return (
     <>
-      <Pokemon></Pokemon>
+      <Lazyapp></Lazyapp>
     </>
   )
 }
