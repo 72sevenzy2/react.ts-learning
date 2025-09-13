ce820a62acb4f2887e59a5c33fc99a20499a2212
72 <72sevenzy2@gmail.com>
2025-09-13 09:09:41 -0800

    updated lazyload.tsx, added  preload functions

--- react-projects/src/components/lazyload-testing/lazyload.tsx
+++ react-projects/src/components/lazyload-testing/lazyload.tsx
@@ -1,6 +1,5 @@
 import type { JSX } from "react";
-
-import { useState, Suspense, lazy } from "react";
+import { lazy, Suspense, useState } from "react";
 
 const Profile = lazy(() => import("./profile"));
 const Desc = lazy(() => import("./desc"));
@@ -8,11 +7,18 @@ const Desc = lazy(() => import("./desc"));
 const Lazyapp = (): JSX.Element => {
     const [int, args] = useState<"desc" | "profile">("desc");
 
+    function preloaddesc() {
+        return import("./desc");
+    }
+
+    function preloadprofile() {
+        return import("./profile");
+    }
+
     return (
         <div>
-            <button onClick={() => args("desc")}>Description</button>
-            <button onClick={() => args("profile")}>Profile</button>
-
+            <button onMouseEnter={preloaddesc} onClick={() => args("desc")}>desc</button>
+            <button onMouseEnter={preloadprofile} onClick={() => args("profile")}>profile</button>
 
             <Suspense fallback={<p>Loading...</p>}>
                 {int === "desc" ? <Desc></Desc> : <Profile></Profile>}
