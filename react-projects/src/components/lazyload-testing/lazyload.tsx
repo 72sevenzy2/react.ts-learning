a76c1802f2eb7bfb2150763134b0720b2d9498f2
72 <72sevenzy2@gmail.com>
2025-09-15 00:46:19 -0800

    added intersection observer

--- react-projects/src/components/lazyload-testing/lazyload.tsx
+++ react-projects/src/components/lazyload-testing/lazyload.tsx
@@ -1,5 +1,5 @@
 import type { JSX } from "react";
-import { Suspense, lazy, useState } from "react";
+import { Suspense, lazy, useState, useEffect, useRef } from "react";
 
 const Profile = lazy(() => import("./profile2"));
 const Desc = lazy(() => import("./desc"));
@@ -14,15 +14,30 @@ function preloaddesc() {
 
 const Lazyapp = (): JSX.Element => {
     const [int, args] = useState<"desc" | "profile">("desc");
+    const [vis, setvis] = useState<boolean>(false);
+    const ref = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+            if (entries[0].isIntersecting) {
+                setvis(true);
+                observer.disconnect();
+            }
+            if (ref.current) { observer.observe(ref.current); }
+
+            return () => observer.disconnect();
+        })
+    }, []);
 
     return (
         <div>
             <button onMouseEnter={preloadprofile} onClick={() => args("profile")}>profile</button>
             <button onMouseEnter={preloaddesc} onClick={() => args("desc")}>desc</button>
-
-            <Suspense fallback={<p>Loading...</p>}>
-                {int === "desc" ? <Desc></Desc> : <Profile></Profile>}
-            </Suspense>
+            <div ref={ref}>
+                <Suspense fallback={<p>Loading...</p>}>
+                    {int === "desc" && vis ? <Desc></Desc> : <Profile></Profile>}
+                </Suspense>
+            </div>
         </div>
     )
 }
