62ebddd2cb391aab8077593a7c04266d44275e03
72 <72sevenzy2@gmail.com>
2025-09-16 07:08:08 -0800

    tested with some basic animations with framer motion in framer.tsx

--- react-projects/src/components/framer-motion/framer.tsx
+++ react-projects/src/components/framer-motion/framer.tsx
@@ -1,9 +1,17 @@
 import type { JSX } from "react";
+import { motion } from "framer-motion";
 
 const Framer = (): JSX.Element => {
     return (
         <div>
-
+            <motion.div
+                whileHover={{ scale: 1.2, backgroundColor: "red" }}
+                drag
+                initial={{ opacity: 0, x: -100, y: 50 }}
+                animate={{ opacity: 1, x: 100 }}
+            >
+                hello
+            </motion.div>
         </div>
     )
 };
