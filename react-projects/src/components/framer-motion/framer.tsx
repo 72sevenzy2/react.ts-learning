2a691243017458c1b02ab34069325908f76c25ac
72 <72sevenzy2@gmail.com>
2025-09-16 07:41:22 -0800

    further experimented with framer motion

--- react-projects/src/components/framer-motion/framer.tsx
+++ react-projects/src/components/framer-motion/framer.tsx
@@ -9,6 +9,13 @@ const Framer = (): JSX.Element => {
                 drag
                 initial={{ opacity: 0, x: -100, y: 50 }}
                 animate={{ opacity: 1, x: 100 }}
+                style={{ background: "red", width: 40, height: 50 }}
+                transition={{
+                    type: "spring",
+                    delay: 0.5,
+                    ease: [0, 1, 1, 0],
+                    repeatType: "mirror"
+                }}
             >
                 hello
             </motion.div>
